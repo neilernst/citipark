d679e3f7cc3932b0f77c31a924cc33af3ab65341
Neil A. Ernst <neil@neilernst.net>
2015-02-21 15:00:23 -0500

    new db

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var db = require("./public/javascripts/get_data");
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/menus/json/all', function(req, res){
+  db.fetchMenus(req, res); //Asynchronous!
 });
 
 app.listen(app.get('port'), function() {
