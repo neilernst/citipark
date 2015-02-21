925d3d8648167020ef907244479a183745d191ab
Neil A. Ernst <neil@neilernst.net>
2015-02-21 16:15:06 -0500

    static files and modified name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('./public/index.html', { root: __dirname });
 });
 
 app.get('/locations/json/all', function(req,res) {
