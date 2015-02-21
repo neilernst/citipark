946eeb32b482c440f0a6bd11c5dee46473b2efde
Neil A. Ernst <neil@neilernst.net>
2015-02-21 15:26:24 -0500

    static serving of JSON files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var db = require("./public/javascripts/get_data");
+var pg = require('pg');
+var conString = "postgres://nernst@localhost/citiparks";
+//var conString = process.env.DATABASE_URL;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -9,10 +12,21 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/locations/json/all', function(req,res) {
+    res.sendFile('./public/locations.json'); //TODO this is static, not db generated.
+});
+
 app.get('/menus/json/all', function(req, res){
-  db.fetchMenus(req, res); //Asynchronous!
+    res.sendFile('./public/menu.json');
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+function fetchMenus(req, res) {
+    console.log(conString);
+    var data =
+    json_fmt  = JSON.stringify(data);
+    res.send(json_fmt);
+}
